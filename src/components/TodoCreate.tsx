9489115525dd446600037301da94893072a3f2d8
sasaki <rs082283@gmail.com>
2023-01-19 02:45:19 +0900

    first

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { todoListState } from "../globalStates/atom";
+import { todoListState } from "../atoms/states";
 import { atom, useRecoilState, useRecoilValue } from "recoil";
 
 export type todoType = { id: number; title: string };
