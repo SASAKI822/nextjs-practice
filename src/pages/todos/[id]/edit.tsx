2f191d7d22064f4a29927a168c9e92491c34aa58
sasaki <rs082283@gmail.com>
2023-01-29 14:39:36 +0900

    edit直し

--- src/pages/todos/[id]/edit.tsx
+++ src/pages/todos/[id]/edit.tsx
@@ -2,34 +2,33 @@ import { todoListState } from "@/atoms/states";
 import { useRouter } from "next/router";
 import { useRecoilState } from "recoil";
 import React, { useState } from "react";
+import Link from "next/link";
 
 const edit = () => {
   const [todoList, setTodoList] = useRecoilState<any>(todoListState);
   const [editText, setEditText] = useState("");
-  console.log(todoList);
-
+  const router = useRouter();
   // 編集機能
   const handleEdit = () => {
     setTodoList((todoList: any) =>
       todoList.map((todoItem: any) =>
-        todoItem.id === router.query.id
-          ? { id: todoItem.id, name: editText }
-          : todoItem
+        todoItem.id === 0 ? { id: todoItem.id, title: editText } : todoItem
       )
     );
+    console.log(router.query.id);
   };
 
   const handleEditChange = (e: any) => {
     setEditText(e.target.value);
   };
-  const router = useRouter();
-  console.log(router.query.id);
+
   console.log(todoList);
   return (
     <>
       {router.query.title}
       <input type="text" value={editText} onChange={handleEditChange} />
       <button onClick={handleEdit}>編集</button>
+      <Link href="/todos">todoリストへ</Link>
     </>
   );
 };
